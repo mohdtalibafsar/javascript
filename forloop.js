a0d6e38980b157444b85f0d974cff8372b444be4
Mohd Talib <mohdtalib.afsar@gmail.com>
2023-01-26 21:23:23 +0530

    for loop

---
+++ forloop.js
@@ -0,0 +1,8 @@
+// Program to add first n natural numbers
+let sum = 0
+let n = prompt("Enter the value of n")
+n = Number.parseInt(n)
+for (let i = 0; i < n; i++) {
+  sum += (i + 1)
+}
+console.log("Sum of first " + n + " natural numbers is " + sum)
